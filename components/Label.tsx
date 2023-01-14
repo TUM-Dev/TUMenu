9ee8b2a531dee4bd5124e07f830037dfde8390de
marinovl7 <la4ezar71@abv.bg>
2023-01-14 17:00:00 +0100

    Reduced the padding and the height of the LabelChips

--- components/Label.tsx
+++ components/Label.tsx
@@ -16,6 +16,7 @@ export default function Label({ bgColor, text }: LabelProps) {
           color: theme.palette.secondary.light,
           fontWeight: '600',
           width: theme.spacing(13),
+          height: theme.spacing(3),
         }}
       />
     </Tooltip>
